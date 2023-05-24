ba5911262b23099932fc62c3678d7b993b5316f8
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-05-24 20:33:36 +0200

    update login

--- login.php
+++ login.php
@@ -17,8 +17,8 @@ $username_error = $password_error = $login_error = '';
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the submitted username and password
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = clean_input($_POST['username']);
+    $password = clean_input($_POST['password']);
 
     // Validate the username
     if (empty($username)) {
@@ -46,30 +46,98 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
+
+// Function to clean user input
+function clean_input($input)
+{
+    // Trim whitespace
+    $input = trim($input);
+    // Remove backslashes
+    $input = stripslashes($input);
+    // Convert special characters to HTML entities
+    $input = htmlspecialchars($input);
+    return $input;
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 300px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        form {
+            margin-top: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            font-weight: bold;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        p.error-message {
+            color: red;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Login</h1>
-    <form method="POST" action="">
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            <p style="color: red;"><?php echo $username_error; ?></p>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <p style="color: red;"><?php echo $password_error; ?></p>
-        </div>
-        <div>
-            <input type="submit" value="Login">
-        </div>
-        <p style="color: red;"><?php echo $login_error; ?></p>
-    </form>
+    <div class="container">
+        <h1>Login</h1>
+        <form method="POST" action="">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div>
+                <input type="submit" value="Login">
+            </div>
+            <p class="error-message"><?php echo $login_error; ?></p>
+        </form>
+    </div>
 </body>
 </html>
