54c64bc938050f2479a41288fb757f82a89ce903
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-05-25 15:40:57 +0200

    title and bold

--- delete-influxdb2.php
+++ delete-influxdb2.php
@@ -12,35 +12,36 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Delete Data</title>
+    <title>Delete InfluxDB2 Metrics</title>
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <div class="container border p-4 mt-5">
+        <h1 style="font-weight: bold;">Delete InfluxDB2 Metrics</h1>
         <form action="delete_data.php" method="post">
           <div class="mb-3">
-            <label for="org" class="form-label">Organization:</label>
+            <label for="org" class="form-label"><b>Organization:</b></label>
             <input type="text" id="org" name="org" class="form-control" value="villaanna">
           </div>
           <div class="mb-3">
-            <label for="bucket" class="form-label">Bucket:</label>
+            <label for="bucket" class="form-label"><b>Bucket:</b></label>
             <input type="text" id="bucket" name="bucket" class="form-control" value="iobroker">
           </div>
             <div class="mb-3">
-                <label for="start" class="form-label">Start:</label>
+                <label for="start" class="form-label"><b>Start:</b></label>
                 <input type="text" id="start" name="start" class="form-control">
             </div>
             <div class="mb-3">
-                <label for="stop" class="form-label">Stop:</label>
+                <label for="stop" class="form-label"><b>Stop:</b></label>
                 <input type="text" id="stop" name="stop" class="form-control">
             </div>
             <div class="mb-3">
-                <label for="measurement" class="form-label">Measurement:</label>
+                <label for="measurement" class="form-label"><b>Measurement:</b></label>
                 <input type="text" id="measurement" name="measurement" class="form-control">
             </div>
             <div class="mb-3">
-                <label for="api_token" class="form-label">API Token:</label>
+                <label for="api_token" class="form-label"><b>API Token:</b></label>
                 <input type="text" id="api_token" name="api_token" class="form-control">
             </div>
             <input type="submit" value="Submit" class="btn btn-primary">
