e191dd8307037684b4c814ff711a5ffc482058d1
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-05-25 10:34:07 +0200

    bootstrap in delete influx

--- delete-influxdb2.php
+++ delete-influxdb2.php
@@ -9,53 +9,46 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 }
 ?>
 
-
 <!-- delete-influxdb2.html -->
-<style>
-  body {
-    font-family: Arial, sans-serif;
-  }
-  form {
-    margin: 0 auto;
-    width: 300px;
-    padding: 1em;
-    border: 1px solid #CCC;
-    border-radius: 1em;
-  }
-  label {
-    display: inline-block;
-    width: 90px;
-    text-align: right;
-  }
-  input[type="text"] {
-    font: 1em sans-serif;
-    width: 130px;
-    box-sizing: border-box;
-    border: 1px solid #999;
-  }
-  input[type="submit"] {
-    margin-left: 120px;
-    padding: .5em .75em;
-    font-size: .8em;
-    border-radius: .5em;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-  }
-</style>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete Data</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <form action="delete_data.php" method="post">
+            <div class="mb-3">
+                <label for="org" class="form-label">Organization:</label>
+                <input type="text" id="org" name="org" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="bucket" class="form-label">Bucket:</label>
+                <input type="text" id="bucket" name="bucket" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="start" class="form-label">Start:</label>
+                <input type="text" id="start" name="start" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="stop" class="form-label">Stop:</label>
+                <input type="text" id="stop" name="stop" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="measurement" class="form-label">Measurement:</label>
+                <input type="text" id="measurement" name="measurement" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="api_token" class="form-label">API Token:</label>
+                <input type="text" id="api_token" name="api_token" class="form-control">
+            </div>
+            <input type="submit" value="Submit" class="btn btn-primary">
+        </form>
+    </div>
 
-<form action="delete_data.php" method="post">
-  <label for="org">Organization:</label>
-  <input type="text" id="org" name="org"><br><br>
-  <label for="bucket">Bucket:</label>
-  <input type="text" id="bucket" name="bucket"><br><br>
-  <label for="start">Start:</label>
-  <input type="text" id="start" name="start"><br><br>
-  <label for="stop">Stop:</label>
-  <input type="text" id="stop" name="stop"><br><br>
-  <label for="measurement">Measurement:</label>
-  <input type="text" id="measurement" name="measurement"><br><br>
-  <label for="api_token">API Token:</label>
-  <input type="text" id="api_token" name="api_token"><br><br>
-  <input type="submit" value="Submit">
-</form>
+    <!-- Include Bootstrap JS (optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
