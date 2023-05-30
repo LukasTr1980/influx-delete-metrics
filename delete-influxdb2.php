ed98f13e8f925f5fe75eeb235293d71e0d6c5f2a
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-05-30 17:20:51 +0200

    responsive for mobile device

--- delete-influxdb2.php
+++ delete-influxdb2.php
@@ -13,6 +13,8 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 <html>
 <head>
     <title>Delete InfluxDB2 Metrics</title>
+    <!-- Include meta viewport tag for responsive page -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
